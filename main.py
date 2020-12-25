b477667e830c6856a87ec92894d969f2a96bf215
yuyumi <changyu2000@gmail.com>
2020-12-25 01:10:14 -0600

    Added for multiple iterations

--- main.py
+++ main.py
@@ -4,50 +4,63 @@ import players
 import notsogreedy
 
 # Constants to determine which method we are using
+MANUAL = -1
 GREEDY = 0
 NOTSOGREEDY = 1
 
 # Allows us to switch between algorithms easily
 def algorithm(hand, deck, method):
     if method == GREEDY:
-        return greedy.algorithm(house, deck)
+        return greedy.algorithm(hand, deck)
     if method == NOTSOGREEDY:
-        return notsogreedy.algorithm(house, deck)
+        return notsogreedy.algorithm(hand, deck)
 
 
-# Main method
+# One iteration of the game
+def game():
+    player, house, deck = cards.start()
 
-player, house, deck = cards.start()
+    timer = -1
+    while timer < 0:
+        # The player always goes first
+        player, deck, player_stop = players.algorithm(player, deck)
+        player_last_settled = cards.settle(player, deck)
+        if player_last_settled >= 0:
+            timer = 1
 
-timer = -1
-while timer < 0:
-    # The player always goes first
-    player, deck, player_stop = players.algorithm(player, deck)
-    player_last_settled = cards.settle(player, deck)
-    if player_last_settled >= 0:
-        timer = 1
-
-    # The house's turn (greedy AI)
-    house, deck, house_stop = algorithm(house, deck, NOTSOGREEDY)
-    house_last_settled = cards.settle(house, deck)
-    if house_last_settled >= 0:
-        timer = 1
+        # The house's turn (greedy AI)
+        house, deck, house_stop = algorithm(house, deck, NOTSOGREEDY)
+        house_last_settled = cards.settle(house, deck)
+        if house_last_settled >= 0:
+            timer = 1
     
-    # Finish the game if both players stop drawing cards
-    if player_stop + house_stop > 1:
-        timer = 1
+        # Finish the game if both players stop drawing cards
+        if player_stop + house_stop > 1:
+            timer = 1
 
-# Print the results of the game
-if player_last_settled > 0:
-    print("Player Lost")
-elif player_last_settled == 0 or house_last_settled > 0:
-    print("Player Won")
-elif house_last_settled == 0:
-    print("Player Lost")
-else:
-    if player.sum() >= house.sum():
-        print("Player Won")
-    else:
+    # Print the results of the game
+    if player_last_settled > 0:
         print("Player Lost")
+        return 0
+    elif player_last_settled == 0 or house_last_settled > 0:
+        print("Player Won")
+        return 1
+    elif house_last_settled == 0:
+        print("Player Lost")
+        return 0
+    else:
+        if player.sum() >= house.sum():
+            print("Player Won")
+            return 1
+        else:
+            print("Player Lost")
+            return 0
+
+# Main method to investigate the performance of the player
+epoches = 10
+wins = 0
+for i in range(epoches):
+    wins += game()
+print(wins/epoches)
 
     
